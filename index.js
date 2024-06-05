3b5d2ad57f6ec19eed3e278d6efcc24febf4a9ac
Avas <avas.bajracharya@renegadeinsurance.com>
2024-06-05 13:55:01 +0545

    feat: first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `
+    type Query {
+        greeting: String
+    }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => "Hello world",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 4000,
+  },
+});
+
+console.log(`Server starting at ${url}`);
